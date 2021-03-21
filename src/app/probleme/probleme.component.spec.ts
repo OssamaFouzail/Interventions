896f5f603afcffc22bc654e311b8d65ad00f2999
Ossama Fouzail <samyfouzail@gmail.com>
2021-03-20 20:54:51 -0400

    test 3

--- src/app/probleme/probleme.component.spec.ts
+++ src/app/probleme/probleme.component.spec.ts
@@ -33,7 +33,11 @@ describe('ProblemeComponent', () => {
     expect(zone.valid).toBeTruthy();
   });
 
-  
+  it('Zone PRÉNOM valide avec 200 caractères', () => {
+    let zone = component.problemForm.controls['prenom'];
+    zone.setValue('a'.repeat(200));
+    expect(zone.valid).toBeTruthy();
+  });
 
   
 
